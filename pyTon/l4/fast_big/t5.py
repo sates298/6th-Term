6571075cc5a0ea669dcd597f6f4f31cba5a978be
sates298 <stanislaw.wo@interia.pl>
2020-04-15 17:20:24 +0200

    finish 5 task python

--- pyTon/l4/fast_big/t5.py
+++ pyTon/l4/fast_big/t5.py
@@ -1,7 +1,9 @@
 import numpy as np
-from math import pi
+from math import pi, log2
 from cmath import exp
 from functools import reduce
+import time
+import random
 
 
 # %%
@@ -9,9 +11,11 @@ from functools import reduce
 
 class FastBigNum:
     def __init__(self, num):
-        print(num)
-        self.number = [int(x) for x in reversed(num)]
         self.length = len(num)
+        self.number = [int(x) for x in reversed(num)]
+        while not log2(self.length).is_integer():
+            self.number.append(0)
+            self.length += 1
 
     def __mul__(self, other):
         global dft, idft
@@ -22,12 +26,12 @@ class FastBigNum:
         Y = dft(other.number, double_n)
         Z = [X[i]*Y[i] for i in range(double_n)]
         result = idft(Z, double_n)
-        value = reduce(lambda x, y: x + y[1]*10**y[0],
+        value = reduce(lambda x, y: x + int(y[1])*10**y[0],
                        enumerate(result), 0)
         return FastBigNum(str(value))
 
     def __str__(self):
-        val = reduce(lambda x, y: x + y[1]*10**y[0],
+        val = reduce(lambda x, y: x + int(y[1])*10**y[0],
                      enumerate(self.number), 0)
         return str(val)
 
@@ -50,6 +54,8 @@ def base_idft(x, n):
 
 
 def fft(x, n):
+    if not log2(n).is_integer():
+        raise ValueError("Length should be power of 2")
     diff = n - len(x)
     x.extend([0 for _ in range(diff)])
     return fft_rec(x)
@@ -73,8 +79,24 @@ def fft_rec(x):
 
 
 def ifft_rec(x):
-    res = fft_rec([i.conjugate() for i in x])
-    return [i.conjugate().real/len(x) for i in res]
+    conj = map(lambda e: e.conjugate(), x)
+    res = fft_rec(list(conj))
+    size = len(res)
+    return [int(round(i.conjugate().real/size)) for i in res]
+
+
+# %% numpy implementation
+
+def numpy_fft(x, n):
+    diff = n - len(x)
+    x.extend([0 for _ in range(diff)])
+    return np.fft.fft(x)
+
+
+def numpy_ifft(x, n):
+    diff = n - len(x)
+    x.extend([0 for _ in range(diff)])
+    return np.round(np.real(np.fft.ifft(x))).astype(int)
 
 
 # %% base tests
@@ -86,22 +108,74 @@ dft = fft
 idft = ifft
 
 # %% numpy tests
-dft = np.fft.fft
-idft = np.fft.ifft
-
-
-# %%
-
-a = [1, 2, 3, 4, 5]
-print(ifft([1, 2, 3, 4, 5, 6, 7, 8], 16))
-print(base_idft([1, 2, 3, 4, 5, 6, 7, 8], 16))
+dft = numpy_fft
+idft = numpy_ifft
 
 
 # %% counting tests
-A = '1312312231232131231231231231231231212331233231349123123123123123'
-B = '1212312311223123121312312321321231231112323123231123123123123123'
-print(len(A))
-a = FastBigNum(A)
-b = FastBigNum(B)
-print((a*b)*(a*b))
-print(int(A)*int(B)*int(A)*int(B))
+def simple_test(size):
+    curr_A = ''.join([random.choice("0123456789") for i in range(size)])
+    curr_B = ''.join([random.choice("0123456789") for i in range(size)])
+    a = FastBigNum(curr_A)
+    b = FastBigNum(curr_B)
+    print(a*b)
+    # print(int(A)*int(B))
+
+
+# simple_test(500_000)
+
+
+# %% tests to file
+file_name = 'fastbignum_benchmark.txt'
+
+
+def avg(x):
+    return sum(x)/len(x)
+
+
+def write_table(table):
+    with open(file_name, 'w') as f:
+        f.write('0. size of nums    1. slow version    2. fast version'
+                '     3. numpy version\n')
+        for s, x, y, z in table:
+            f.write(f'{s}: {x} {y} {z}\n')
+
+
+def generate_element(size):
+    global dft, idft
+    samples_no = 10
+    base, fast, nump = [], [], []
+
+    for _ in range(samples_no):
+        curr_A = ''.join([random.choice("0123456789") for i in range(size)])
+        curr_B = ''.join([random.choice("0123456789") for i in range(size)])
+        a = FastBigNum(curr_A)
+        b = FastBigNum(curr_B)
+        dft = base_dft
+        idft = base_idft
+        start = time.time()
+        a*b
+        base.append(time.time() - start)
+        dft = fft
+        idft = ifft
+        start = time.time()
+        a*b
+        fast.append(time.time() - start)
+        dft = numpy_fft
+        idft = numpy_ifft
+        start = time.time()
+        a*b
+        nump.append(time.time() - start)
+
+    return (size, avg(base), avg(fast), avg(nump))
+
+
+def final_test():
+    table = [generate_element(100_000),
+             generate_element(500_000),
+             generate_element(1_000_000)]
+    write_table(table)
+
+
+# final_test()
+# generate_element(1000)
